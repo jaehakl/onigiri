8225adecd2ec75808961a10eaff8e4ab94fa596b
jaehakl <leejaehak87@gmail.com>
2025-09-08 00:30:07 +0900

    Update run-admin.bat

--- run-admin.bat
+++ run-admin.bat
@@ -1,11 +1,11 @@
 @echo off
 REM 백엔드 실행 (새 창)
-cd ./apps/jpkr-admin/api
-start cmd /k "call run.bat"
+cd ./apps/jpkr/api
+start cmd /k "call creator.bat"
 cd ../../..
 
 REM 프론트엔드 실행 (새 창)
-cd ./apps/jpkr-admin/ui   
+cd ./apps/jpkr/_creator-ui   
 start cmd /k "call run.bat"
 cd ../../..
 
