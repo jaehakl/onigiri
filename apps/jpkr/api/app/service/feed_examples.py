0a39ff270d59fa6555d6d92fd60da20b14039b0c
jaehakl <leejaehak87@gmail.com>
2025-11-27 15:17:12 +0900

    ex

--- apps/jpkr/api/app/service/feed_examples.py
+++ apps/jpkr/api/app/service/feed_examples.py
@@ -8,7 +8,7 @@ from methods.words_from_examples_batch import words_from_examples_batch
 
 def get_examples_for_user(db: Session = None, tags: List[str] = None, user_id: str = None) -> List[Example]:
     if user_id is not None:
-        examples = recommend_examples_worst_reading(limit_examples=6, tags=tags, db=db, user_id=user_id)
+        examples = recommend_examples_worst_reading(limit_examples=12, tags=tags, db=db, user_id=user_id)
     else:
         examples = recommend_examples_simple(limit_examples=6, tags=tags, db=db)
     examples_result = list(words_from_examples_batch(examples, db=db, user_id=user_id).values())
